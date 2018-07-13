3bf11563ab1720f915988e25a383df354cb42303
Vinci-da-Game <11582145@student.uts.edu.au>
2018-07-13 14:51:51 +1000

    feat: setup form.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,6 +1,6 @@
 // import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormArray, Validators, FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
 	selector: 'app-header',
@@ -10,11 +10,30 @@ export class HeaderComponent implements OnInit {
 
 	searchForm: FormGroup;
 	searchTermInput: FormControl;
+	private ALLWHITESPACE = /\S/;
 
 	constructor(
 		private _fbr: FormBuilder,
 	) { }
 
 	ngOnInit() {
+		this.createSearchFormControl();
+		this.createSearchFormGroup();
+	}
+
+	private createSearchFormControl() {
+		const initTerm = null;
+		const validatorArr = [Validators.required, Validators.pattern(this.ALLWHITESPACE)];
+		this.searchTermInput = new FormControl(initTerm, validatorArr);
+	}
+
+	private createSearchFormGroup() {
+		this.searchForm = new FormGroup({
+			'searchTermInput': this.searchTermInput
+		});
+	}
+
+	onSubmit() {
+
 	}
 }
